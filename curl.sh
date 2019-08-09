a53738c6ef2dadf93308e399a3868264536d6173
ak1t4 <know.0nix@gmail.com>
2019-08-09 18:07:14 -0300

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-while read -r line
-do
+echo "###############################"
+echo "# Http response with multi    #"
+echo "# threadhing support via Curl #"
+echo "# by ak1t4                    #"
+echo "###############################"
 
- echo $line && curl --silent --connect-timeout 8 --output /dev/null https://$line/ -I -w "%{http_code}\n"
-done < url.lists
+echo "requires: parallel gnu"
+echo "just run:"
+echo "./curl.sh yours-urls.txt"
+echo "output is over urlstatus.txt"
+echo ""
+
+doit() {
+    url="$1"
+    urlstatus=$(curl -o /dev/null --insecure --silent --head --write-out  '%{http_code}' "${url}" --max-time 5 ) &&
+    echo "$url  $urlstatus"
+}
+export -f doit
+salida=(`parallel  -j200 doit :::: $1 >> urlstatus.txt`) > /dev/null
